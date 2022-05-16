b23f55fdcba03a1137b5bd92d67a255c0c7d114a
Alex Fernan <mercadoalexfernan@gmail.com>
2022-05-16 23:04:44 +0800

    Add project files.

---
+++ MyCollectionSite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCollectionSite.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace MyCollectionSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using var jsonFile = System.IO.
+                File.OpenRead("Data/collection.json");
+
+            var items = JsonSerializer
+                .Deserialize<CollectionItem[]>(jsonFile);
+
+            return View(items);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
